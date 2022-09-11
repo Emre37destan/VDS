247259fb7443e97e67c43de912377d4e9df6eef4
Kral <108046740+Emre37destan@users.noreply.github.com>
2022-09-11 23:32:00 +0300

    KralDev
    KralDev

--- start.bat
+++ start.bat
@@ -3,8 +3,8 @@ del /f "C:\Users\Public\Desktop\Epic Games Launcher.lnk" > out.txt 2>&1
 net config server /srvcomment:"Win10 Kral" > out.txt 2>&1
 REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer" /V EnableAutoTray /T REG_DWORD /D 0 /F > out.txt 2>&1
 REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" /f /v Wallpaper /t REG_SZ /d D:\a\wallpaper.bat
-net user Administrator test123 /add >nul
-net localgroup administrators Administrator /add >nul
+net user administrator test123 /add >nul
+net localgroup administrators administrator /add >nul
 net user administrator /active:yes >nul
 net user installer /delete
 diskperf -Y >nul
@@ -19,7 +19,7 @@ echo Sorun Yasarsaniz; Emre 31 ⭐ Kral#0156
 echo BILGI RDP
 echo IP:
 tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Failed to retreive NGROK authtoken - check again your authtoken"
-echo Kullanici Adi: Administrator
+echo Kullanici Adi: administrator
 echo Sifre: test123
 echo Simdi giris yapabilirsiniz!
 ping -n 10 127.0.0.1 >nul
